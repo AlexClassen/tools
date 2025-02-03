35413e9188f64557352e1082ea0fef67024aedde
AlexClassen <classenalex@pt.lu>
2025-02-04 00:03:10 +0100

    Added CodeSnippets Page

---
+++ src/pages/CodeSnippets.tsx
@@ -0,0 +1,9 @@
+const CodeSnippets = () => {
+  return (
+    <div className="p-6">
+      <h1 className="text-2xl font-bold mb-4">Code Snippets</h1>
+    </div>
+  );
+};
+
+export default CodeSnippets;
